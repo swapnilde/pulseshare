c25305e85e977472ebc4a4b4e0abfdfcf3f7344f
swapnilde <swwapnildeshpande@gmail.com>
2026-02-19 16:35:26 +0530

    refactor: Convert album and list embed block edit components from class to functional components, utilizing `useEffect` for lifecycle management.

--- blocks/album-embed/edit.js
+++ blocks/album-embed/edit.js
@@ -5,14 +5,12 @@ import {
 	PanelBody,
 	__experimentalUnitControl as UnitControl,
 } from '@wordpress/components';
-import { InspectorControls } from '@wordpress/block-editor';
-const { Component } = wp.element;
-
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { useEffect } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
 import classnames from 'classnames';
 
 import './editor.scss';
-
-import apiFetch from '@wordpress/api-fetch';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -20,202 +18,187 @@ import apiFetch from '@wordpress/api-fetch';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
  *
- * @return {JSX} Element to render.
+ * @param {Object} props Block props.
+ * @return {JSX.Element} Element to render.
  */
-export default class albumEmbedEdit extends Component {
-	componentDidMount() {
-		const { attributes, setAttributes, clientId } = this.props;
-		const { blockID, albumArray } = attributes;
+export default function AlbumEmbedEdit({ attributes, setAttributes, clientId, className }) {
+	const {
+		blockID,
+		albumArray,
+		displayType,
+		currentTrack,
+		height,
+		width,
+	} = attributes;
 
+	// Set block ID on mount.
+	useEffect(() => {
 		if (!blockID) {
 			setAttributes({ blockID: `album-embed-${clientId}` });
 		}
+	}, [blockID, clientId, setAttributes]);
 
-		if (0 === albumArray.length) {
-			this.initAlbum();
-		}
-
-		// Fetch tracks via the server-side REST API proxy (credentials stay server-side).
+	// Fetch tracks via the server-side REST API proxy.
+	useEffect(() => {
 		apiFetch({ path: '/pulseshare/v1/tracks' })
 			.then((tracks) => {
-				setAttributes({
-					albumArray: tracks,
-				});
+				setAttributes({ albumArray: tracks });
 			})
 			.catch((error) => {
 				console.error('PulseShare: Failed to fetch tracks', error);
 			});
-	}
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-	initAlbum() {
-		const { setAttributes } = this.props;
-		setAttributes({
-			albumArray: [],
-		});
-	}
+	const classes = classnames(className, 'album-embed');
 
-	render() {
-		const { attributes, setAttributes, className } = this.props;
-		const {
-			blockID,
-			albumArray,
-			displayType,
-			currentTrack,
-			height,
-			width,
-		} = attributes;
+	return (
+		<>
+			<InspectorControls>
+				<div className="sfwe-block-sidebar">
+					<PanelBody
+						title={__('Settings', 'pulseshare')}
+						initialOpen={true}
+					>
+						<RadioControl
+							label={__('Display Type', 'pulseshare')}
+							help="Select the display type for the album."
+							selected={displayType ? displayType : 'full'}
+							options={[
+								{ label: 'Full Album', value: 'full' },
+								{ label: 'Single Track', value: 'single' },
+							]}
+							onChange={(type) => {
+								setAttributes({ displayType: type });
+							}}
+						/>
 
-		const classes = classnames(className, 'album-embed');
-
-		return (
-			<>
-				<InspectorControls>
-					<div className="sfwe-block-sidebar">
-						<PanelBody
-							title={__('Settings', 'pulseshare')}
-							initialOpen={true}
-						>
-							<RadioControl
-								label={__('Display Type', 'pulseshare')}
-								help="Select the display type for the album."
-								selected={displayType ? displayType : 'full'}
-								options={[
-									{ label: 'Full Album', value: 'full' },
-									{ label: 'Single Track', value: 'single' },
-								]}
-								onChange={(type) => {
-									setAttributes({ displayType: type });
+						{displayType === 'single' && (
+							<SelectControl
+								__nextHasNoMarginBottom
+								label={__('Select Track', 'pulseshare')}
+								help="Selected track will be displayed in the frontend."
+								value={
+									currentTrack
+										? currentTrack.id
+										: albumArray[0]?.id
+								}
+								options={albumArray.map((track) => {
+									return {
+										label: track.name,
+										value: track.id,
+									};
+								})}
+								onChange={(id) => {
+									setAttributes({
+										currentTrack: albumArray.find(
+											(track) => track.id === id
+										),
+									});
 								}}
 							/>
+						)}
 
-							{displayType === 'single' && (
-								<SelectControl
-									__nextHasNoMarginBottom
-									label={__('Select Track', 'pulseshare')}
-									help="Selected track will be displayed in the frontend."
-									value={
-										currentTrack
-											? currentTrack.id
-											: albumArray[0].id
-									}
-									options={albumArray.map((episode) => {
-										return {
-											label: episode.name,
-											value: episode.id,
-										};
-									})}
-									onChange={(id) => {
-										setAttributes({
-											currentTrack: albumArray.find(
-												(episode) => episode.id === id
-											),
-										});
-									}}
-								/>
-							)}
+						<UnitControl
+							__next40pxDefaultSize
+							label="Height"
+							onChange={(value) => {
+								setAttributes({ height: value });
+							}}
+							units={[
+								{
+									a11yLabel: 'Pixels (px)',
+									label: 'px',
+									step: 1,
+									value: 'px',
+								},
+								{
+									a11yLabel: 'Percent (%)',
+									label: '%',
+									step: 1,
+									value: '%',
+								},
+							]}
+							value={height}
+						/>
+						<UnitControl
+							__next40pxDefaultSize
+							label="Width"
+							onChange={(value) => {
+								setAttributes({ width: value });
+							}}
+							units={[
+								{
+									a11yLabel: 'Pixels (px)',
+									label: 'px',
+									step: 1,
+									value: 'px',
+								},
+								{
+									a11yLabel: 'Percent (%)',
+									label: '%',
+									step: 1,
+									value: '%',
+								},
+							]}
+							value={width}
+						/>
+					</PanelBody>
+				</div>
+			</InspectorControls>
+			<div className={classes} id={blockID}>
+				<div className="container">
+					<div className={'sfwe-episode'}>
+						{displayType === 'single' && !currentTrack?.id && (
+							<div className="notice notice-info alt">
+								<p>
+									<i>
+										{__(
+											'Please select a track from the block settings.',
+											'pulseshare'
+										)}
+									</i>
+								</p>
+							</div>
+						)}
 
-							<UnitControl
-								__next40pxDefaultSize
-								label="Height"
-								onChange={(value) => {
-									setAttributes({ height: value });
-								}}
-								units={[
-									{
-										a11yLabel: 'Pixels (px)',
-										label: 'px',
-										step: 1,
-										value: 'px',
-									},
-									{
-										a11yLabel: 'Percent (%)',
-										label: '%',
-										step: 1,
-										value: '%',
-									},
-								]}
-								value={height}
-							/>
-							<UnitControl
-								__next40pxDefaultSize
-								label="Width"
-								onChange={(value) => {
-									setAttributes({ width: value });
-								}}
-								units={[
-									{
-										a11yLabel: 'Pixels (px)',
-										label: 'px',
-										step: 1,
-										value: 'px',
-									},
-									{
-										a11yLabel: 'Percent (%)',
-										label: '%',
-										step: 1,
-										value: '%',
-									},
-								]}
-								value={width}
-							/>
-						</PanelBody>
-					</div>
-				</InspectorControls>
-				<div className={classes} id={blockID}>
-					<div className="container">
-						<div className={'sfwe-episode'}>
-							{displayType === 'single' && !currentTrack.id && (
-								<div className="notice notice-info alt">
-									<p>
-										<i>
-											{__(
-												'Please select a track from the block settings.',
-												'pulseshare'
-											)}
-										</i>
-									</p>
-								</div>
-							)}
-
-							{displayType === 'single' && currentTrack.id && (
-								<iframe
-									id={'sfwe-track-' + currentTrack.id}
-									frameBorder="0"
-									allowFullScreen=""
-									allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-									loading="lazy"
-									width={width ? width : '100%'}
-									height={height ? height : '200'}
-									src={
-										'https://open.spotify.com/embed/track/' +
-										currentTrack.id
-									}
-								></iframe>
-							)}
-							{displayType === 'full' && (
-								<iframe
-									id={
-										'sfwe-album-' +
-										PulseShareAdminVars.pulseshare_options
-											.album_id
-									}
-									frameBorder="0"
-									allowFullScreen=""
-									allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-									loading="lazy"
-									width={width ? width : '100%'}
-									height={height ? height : '380'}
-									src={
-										'https://open.spotify.com/embed/album/' +
-										PulseShareAdminVars.pulseshare_options
-											.album_id
-									}
-								></iframe>
-							)}
-						</div>
+						{displayType === 'single' && currentTrack?.id && (
+							<iframe
+								id={'sfwe-track-' + currentTrack.id}
+								frameBorder="0"
+								allowFullScreen=""
+								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+								loading="lazy"
+								width={width ? width : '100%'}
+								height={height ? height : '200'}
+								src={
+									'https://open.spotify.com/embed/track/' +
+									currentTrack.id
+								}
+							></iframe>
+						)}
+						{displayType === 'full' && (
+							<iframe
+								id={
+									'sfwe-album-' +
+									PulseShareAdminVars.pulseshare_options
+										.album_id
+								}
+								frameBorder="0"
+								allowFullScreen=""
+								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+								loading="lazy"
+								width={width ? width : '100%'}
+								height={height ? height : '380'}
+								src={
+									'https://open.spotify.com/embed/album/' +
+									PulseShareAdminVars.pulseshare_options
+										.album_id
+								}
+							></iframe>
+						)}
 					</div>
 				</div>
-			</>
-		);
-	}
+			</div>
+		</>
+	);
 }
--- blocks/list-embed/edit.js
+++ blocks/list-embed/edit.js
@@ -6,14 +6,12 @@ import {
 	PanelBody,
 	__experimentalUnitControl as UnitControl,
 } from '@wordpress/components';
-import { InspectorControls } from '@wordpress/block-editor';
-const { Component } = wp.element;
-
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { useEffect } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
 import classnames from 'classnames';
 
 import './editor.scss';
-
-import apiFetch from '@wordpress/api-fetch';
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -21,233 +19,217 @@ import apiFetch from '@wordpress/api-fetch';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
  *
- * @return {JSX} Element to render.
+ * @param {Object} props Block props.
+ * @return {JSX.Element} Element to render.
  */
-export default class listEmbedEdit extends Component {
-	componentDidMount() {
-		const { attributes, setAttributes, clientId } = this.props;
-		const { blockID, episodesArray } = attributes;
+export default function ListEmbedEdit({ attributes, setAttributes, clientId, className }) {
+	const {
+		blockID,
+		episodesArray,
+		displayType,
+		currentEpisode,
+		isVideo,
+		height,
+		width,
+	} = attributes;
 
+	// Set block ID on mount.
+	useEffect(() => {
 		if (!blockID) {
 			setAttributes({ blockID: `list-embed-${clientId}` });
 		}
+	}, [blockID, clientId, setAttributes]);
 
-		if (0 === episodesArray.length) {
-			this.initEpisodes();
-		}
-
-		// Fetch episodes via the server-side REST API proxy (credentials stay server-side).
+	// Fetch episodes via the server-side REST API proxy.
+	useEffect(() => {
 		apiFetch({ path: '/pulseshare/v1/episodes' })
 			.then((episodes) => {
-				setAttributes({
-					episodesArray: episodes,
-				});
+				setAttributes({ episodesArray: episodes });
 			})
 			.catch((error) => {
 				console.error('PulseShare: Failed to fetch episodes', error);
 			});
-	}
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-	initEpisodes() {
-		const { setAttributes } = this.props;
-		setAttributes({
-			episodesArray: [],
-		});
-	}
+	const classes = classnames(className, 'list-embed');
+	const video = isVideo ? 'video' : '';
 
-	render() {
-		const { attributes, setAttributes, className } = this.props;
-		const {
-			blockID,
-			episodesArray,
-			displayType,
-			currentEpisode,
-			isVideo,
-			height,
-			width,
-		} = attributes;
+	return (
+		<>
+			<InspectorControls>
+				<div className="sfwe-block-sidebar">
+					<PanelBody
+						title={__('Settings', 'pulseshare')}
+						initialOpen={true}
+					>
+						<RadioControl
+							label={__('Display Type', 'pulseshare')}
+							help="Select the display type for the episode."
+							selected={displayType ? displayType : 'full'}
+							options={[
+								{ label: 'Full Show', value: 'full' },
+								{
+									label: 'Single Episode',
+									value: 'single',
+								},
+							]}
+							onChange={(type) => {
+								setAttributes({ displayType: type });
+							}}
+						/>
 
-		const classes = classnames(className, 'list-embed');
-
-		const video = isVideo ? 'video' : '';
-
-		return (
-			<>
-				<InspectorControls>
-					<div className="sfwe-block-sidebar">
-						<PanelBody
-							title={__('Settings', 'pulseshare')}
-							initialOpen={true}
-						>
-							<RadioControl
-								label={__('Display Type', 'pulseshare')}
-								help="Select the display type for the episode."
-								selected={displayType ? displayType : 'full'}
-								options={[
-									{ label: 'Full Show', value: 'full' },
-									{
-										label: 'Single Episode',
-										value: 'single',
-									},
-								]}
-								onChange={(type) => {
-									setAttributes({ displayType: type });
-								}}
-							/>
-
-							{displayType === 'single' && (
-								<SelectControl
-									__nextHasNoMarginBottom
-									label={__(
-										'Select Episode',
-										'pulseshare'
-									)}
-									help="Selected episode will be displayed in the frontend."
-									value={
-										currentEpisode
-											? currentEpisode.id
-											: episodesArray[0].id
-									}
-									options={episodesArray.map(
-										(episode) => {
-											return {
-												label: episode.name,
-												value: episode.id,
-											};
-										}
-									)}
-									onChange={(id) => {
-										setAttributes({
-											currentEpisode: episodesArray.find(
-												(episode) => episode.id === id
-											),
-										});
-									}}
-								/>
-							)}
-
-							{displayType === 'single' && (
-								<ToggleControl
-									__nextHasNoMarginBottom
-									checked={isVideo ? isVideo : false}
-									help={__(
-										'Enable this option if this episode is a video.',
-										'pulseshare'
-									)}
-									label={__(
-										'Is this a video episode?',
-										'pulseshare'
-									)}
-									onChange={(state) => {
-										setAttributes({ isVideo: state });
-									}}
-								/>
-							)}
-
-							<UnitControl
-								__next40pxDefaultSize
-								label="Height"
-								onChange={(value) => {
-									setAttributes({ height: value });
-								}}
-								units={[
-									{
-										a11yLabel: 'Pixels (px)',
-										label: 'px',
-										step: 1,
-										value: 'px',
-									},
-									{
-										a11yLabel: 'Percent (%)',
-										label: '%',
-										step: 1,
-										value: '%',
-									},
-								]}
-								value={height}
-							/>
-							<UnitControl
-								__next40pxDefaultSize
-								label="Width"
-								onChange={(value) => {
-									setAttributes({ width: value });
-								}}
-								units={[
-									{
-										a11yLabel: 'Pixels (px)',
-										label: 'px',
-										step: 1,
-										value: 'px',
-									},
-									{
-										a11yLabel: 'Percent (%)',
-										label: '%',
-										step: 1,
-										value: '%',
-									},
-								]}
-								value={width}
-							/>
-						</PanelBody>
-					</div>
-				</InspectorControls>
-
-				<div className={classes} id={blockID}>
-					<div className="container">
-						<div className={'sfwe-episode'}>
-							{displayType === 'single' &&
-								!currentEpisode.id && (
-									<div className="notice notice-info alt">
-										<p>
-											<i>
-												{__(
-													'Please select an episode from the block settings.',
-													'pulseshare'
-												)}
-											</i>
-										</p>
-									</div>
+						{displayType === 'single' && (
+							<SelectControl
+								__nextHasNoMarginBottom
+								label={__(
+									'Select Episode',
+									'pulseshare'
 								)}
+								help="Selected episode will be displayed in the frontend."
+								value={
+									currentEpisode
+										? currentEpisode.id
+										: episodesArray[0]?.id
+								}
+								options={episodesArray.map(
+									(episode) => {
+										return {
+											label: episode.name,
+											value: episode.id,
+										};
+									}
+								)}
+								onChange={(id) => {
+									setAttributes({
+										currentEpisode: episodesArray.find(
+											(episode) => episode.id === id
+										),
+									});
+								}}
+							/>
+						)}
 
-							{displayType === 'single' && currentEpisode.id && (
-								<iframe
-									id={'sfwe-episode-' + currentEpisode.id}
-									frameBorder="0"
-									allowFullScreen=""
-									allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-									loading="lazy"
-									width={width ? width : '100%'}
-									height={height ? height : '200'}
-									src={
-										'https://open.spotify.com/embed/episode/' +
-										currentEpisode.id +
-										'/' +
-										video
-									}
-								></iframe>
+						{displayType === 'single' && (
+							<ToggleControl
+								__nextHasNoMarginBottom
+								checked={isVideo ? isVideo : false}
+								help={__(
+									'Enable this option if this episode is a video.',
+									'pulseshare'
+								)}
+								label={__(
+									'Is this a video episode?',
+									'pulseshare'
+								)}
+								onChange={(state) => {
+									setAttributes({ isVideo: state });
+								}}
+							/>
+						)}
+
+						<UnitControl
+							__next40pxDefaultSize
+							label="Height"
+							onChange={(value) => {
+								setAttributes({ height: value });
+							}}
+							units={[
+								{
+									a11yLabel: 'Pixels (px)',
+									label: 'px',
+									step: 1,
+									value: 'px',
+								},
+								{
+									a11yLabel: 'Percent (%)',
+									label: '%',
+									step: 1,
+									value: '%',
+								},
+							]}
+							value={height}
+						/>
+						<UnitControl
+							__next40pxDefaultSize
+							label="Width"
+							onChange={(value) => {
+								setAttributes({ width: value });
+							}}
+							units={[
+								{
+									a11yLabel: 'Pixels (px)',
+									label: 'px',
+									step: 1,
+									value: 'px',
+								},
+								{
+									a11yLabel: 'Percent (%)',
+									label: '%',
+									step: 1,
+									value: '%',
+								},
+							]}
+							value={width}
+						/>
+					</PanelBody>
+				</div>
+			</InspectorControls>
+
+			<div className={classes} id={blockID}>
+				<div className="container">
+					<div className={'sfwe-episode'}>
+						{displayType === 'single' &&
+							!currentEpisode?.id && (
+								<div className="notice notice-info alt">
+									<p>
+										<i>
+											{__(
+												'Please select an episode from the block settings.',
+												'pulseshare'
+											)}
+										</i>
+									</p>
+								</div>
 							)}
-							{displayType === 'full' && (
-								<iframe
-									id={
-										'sfwe-show-' +
-										PulseShareAdminVars.pulseshare_options.show_id
-									}
-									frameBorder="0"
-									allowFullScreen=""
-									allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-									loading="lazy"
-									width={width ? width : '100%'}
-									height={height ? height : '200'}
-									src={
-										'https://open.spotify.com/embed/show/' +
-										PulseShareAdminVars.pulseshare_options.show_id
-									}
-								></iframe>
-							)}
-						</div>
+
+						{displayType === 'single' && currentEpisode?.id && (
+							<iframe
+								id={'sfwe-episode-' + currentEpisode.id}
+								frameBorder="0"
+								allowFullScreen=""
+								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+								loading="lazy"
+								width={width ? width : '100%'}
+								height={height ? height : '200'}
+								src={
+									'https://open.spotify.com/embed/episode/' +
+									currentEpisode.id +
+									'/' +
+									video
+								}
+							></iframe>
+						)}
+						{displayType === 'full' && (
+							<iframe
+								id={
+									'sfwe-show-' +
+									PulseShareAdminVars.pulseshare_options.show_id
+								}
+								frameBorder="0"
+								allowFullScreen=""
+								allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+								loading="lazy"
+								width={width ? width : '100%'}
+								height={height ? height : '200'}
+								src={
+									'https://open.spotify.com/embed/show/' +
+									PulseShareAdminVars.pulseshare_options.show_id
+								}
+							></iframe>
+						)}
 					</div>
 				</div>
-			</>
-		);
-	}
+			</div>
+		</>
+	);
 }
